d11b5f3efdc1f4e7d7737df02031b8b1448bbeff
Alexandr Gorlov <a.gorlov@gmail.com>
2018-08-04 13:33:01 +0300

    usage example: example.php

---
+++ example.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Example script
+ *
+ * Open console or terminal and run:
+ *
+ * ```bash
+ * php ./example.php
+ * ```
+ *
+ * @author Alexandr Gorlov <a.gorlov@gmail.com>
+ */
+require_once 'Msg.php';
+require_once 'DiscordMsg.php';
+
+
+echo "To see dumb messages..\n";
+echo "..join the discordmsg chanel https://discord.gg/Bh4EZB and enjoy!\n";
+echo "Feel free to test!\n";
+
+
+// First message
+$msg = new \AG\DiscordMsg('Hello, Friends');
+$msg->send();
+
+
+// Second message
+(new \AG\DiscordMsg(
+    'I started the example.php, something happened?',
+    null,
+    $_ENV["USER"] ?? 'Mr. Pitkin'
+))->send();
+
+
+// Third message
+(new \AG\DiscordMsg(
+    "Maybe the missile is launched? :grimacing:", // message
+    'https://discordapp.com/api/webhooks/475239272610398218/' . // chanel webhook link
+        'tfpeg5EFI_94oEHfltZwcB5UAurfcswkBw5mKouZCq-akhvxe5BNOtN16AqSHvmSwpum',
+    get_current_user(), // bot name
+    '' // avatar url
+))->send();
+
